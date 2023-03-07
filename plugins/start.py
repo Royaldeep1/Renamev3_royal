ac7efcb90b1d724a7915063e7d921f81a75a186b
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-03-07 11:51:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -186,6 +186,6 @@ async def send_doc(client, message):
             fileid = file.file_id
             total_rename(int(botid), prrename)
             total_size(int(botid), prsize, file.file_size)
-            await message.reply_text(f"""__What do you want me to do with this file?__\n\n**📁 File Name** :- {filename}\n\n**⚙️ File Size** :- {filesize}\n**📡 Your Data center** :- {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(
+            await message.reply_text(f"""__What do you want me to do with this file?__\n\n**📁 File Name** :- {filename}\n\n**⚙️ File Size** :- {filesize}\n\n**📡 Your Data center** :- {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("✏️ Rename", callback_data="rename"),
                   InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]]))
